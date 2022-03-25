2185d6d08dd121a1cf8e36203849252834c9a969
Wei Dai <greendaiwei@gmail.com>
2022-03-24 22:39:36 -0400

    finished AbstractMelonOrder class

--- melons.py
+++ melons.py
@@ -1,7 +1,22 @@
 """Classes for melon orders."""
 
+class AbstractMelonOrder:
+    """An abstract menlon order class"""
+    def get_total(self):
+        """Calculate price, including tax."""
 
-class DomesticMelonOrder:
+        base_price = 5
+        total = (1 + self.tax) * self.qty * base_price
+
+        return total
+
+    def mark_shipped(self):
+        """Record the fact than an order has been shipped."""
+
+        self.shipped = True
+
+        
+class DomesticMelonOrder(AbstractMelonOrder):
     """A melon order within the USA."""
 
     def __init__(self, species, qty):
@@ -21,13 +36,9 @@ class DomesticMelonOrder:
 
         return total
 
-    def mark_shipped(self):
-        """Record the fact than an order has been shipped."""
-
-        self.shipped = True
 
 
-class InternationalMelonOrder:
+class InternationalMelonOrder(AbstractMelonOrder):
     """An international (non-US) melon order."""
 
     def __init__(self, species, qty, country_code):
@@ -39,19 +50,6 @@ class InternationalMelonOrder:
         self.shipped = False
         self.order_type = "international"
         self.tax = 0.17
-
-    def get_total(self):
-        """Calculate price, including tax."""
-
-        base_price = 5
-        total = (1 + self.tax) * self.qty * base_price
-
-        return total
-
-    def mark_shipped(self):
-        """Record the fact than an order has been shipped."""
-
-        self.shipped = True
 
     def get_country_code(self):
         """Return the country code."""
